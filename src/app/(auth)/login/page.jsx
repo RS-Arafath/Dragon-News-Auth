e19918844991681b350d91d1b125077b7a876e94
Arafath <rsarafath.signin@gmail.com>
2026-06-26 23:26:39 +0600

    feat: update login page layout and styling for improved user experience

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 
 const LogInPage = () => {
   return (
-    <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100 my-6 sm:my-8 lg:my-10">
-      <div className="p-4 rounded-xl bg-white">
-        <h2 className="font-bold text-xl sm:text-3xl text-center">
+    <div className="container mx-auto  flex justify-center items-center my-6 sm:my-8 lg:my-10">
+      <div className="p-2 sm:p-8  rounded-xl  bg-slate-100">
+        <h2 className="font-bold text-lg sm:text-2xl  text-left">
           Login Your Account
         </h2>
-        <div>
-          <form>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+        <div className="border my-2 sm:my-4 border-gray-200"></div>
+        <div className=" p-2 sm:p-4 md:p-6 ">
+          <form className="">
+            <fieldset className="fieldset bg-base-200 w-xs sm:w-lg border-base-300 rounded-box text-lg border  p-5">
               <legend className="fieldset-legend">Login</legend>
 
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                className="input w-full"
+                placeholder="Email"
+              />
 
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                type="password"
+                className="input w-full"
+                placeholder="Password"
+              />
 
               <button className="btn btn-neutral mt-4">Login</button>
             </fieldset>
