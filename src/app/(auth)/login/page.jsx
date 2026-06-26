ceccf71e2efbc39679e25225338a5026e58b9a96
Arafath <rsarafath.signin@gmail.com>
2026-06-26 11:39:34 +0600

    feat: add Heroui dependencies and update login page styling
    - Added @heroui/react and @heroui/styles to dependencies in package.json.
    - Introduced @gravity-ui/icons to devDependencies for enhanced icon support.
    - Enhanced the login page layout with a centered card design and updated heading for better user experience.
    - Imported Heroui styles in globals.css for consistent styling across the application.

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const LogInPage = () => {
   return (
-    <div className='container mx-auto'>
-      Login
+    <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100 my-6 sm:my-8 lg:my-8">
+      <div className="p-4 rounded-xl bg-white">
+        <h2 className="font-bold text-xl sm:text-3xl text-center">Login Your Account</h2>
+      </div>
     </div>
   );
 };
