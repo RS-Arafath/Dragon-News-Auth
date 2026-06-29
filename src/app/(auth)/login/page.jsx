6e3e5e37f99b8a55c08e104010040e85ff550321
Arafath <rsarafath.signin@gmail.com>
2026-06-29 09:45:19 +0600

    feat: enhance login functionality with improved password validation and authentication integration

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { authClient } from '@/lib/auth-client';
 import { Description } from '@heroui/react';
 import Link from 'next/link';
 import React from 'react';
@@ -12,40 +13,47 @@ const LogInPage = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const handleFormSubmit = (data) => {
-    console.log(data);
+  const handleFormSubmit = async(data) => {
+    const { email, password } = data;
 
     // e.preventDefault();
 
     // const email = e.target.email.value;
     // const password = e.target.password.value;
     // console.log(email, password);
-     if (data.password.length < 6) {
-       alert('Password must be at least 8 characters');
-       return;
-     }
-
-     if (!/[A-Z]/.test(data.password)) {
-       alert('Password must contain at least one uppercase letter');
-       return;
-     }
-
-     if (!/[0-9]/.test(data.password)) {
-       alert('Password must contain at least one number');
-       return;
-     }
-
-    alert('Login Success');
-
-    const success = true;
-
-    if (success) {
-      reset(); //
+    if (data.password.length < 8) {
+      alert('Password must be at least 8 characters');
+      return;
     }
+
+    if (!/[A-Z]/.test(data.password)) {
+      alert('Password must contain at least one uppercase letter');
+      return;
+    }
+
+    if (!/[0-9]/.test(data.password)) {
+      alert('Password must contain at least one number');
+      return;
+    }
+    const { data: res, error } = await authClient.signIn.email({
+      email: email,
+      password: password,
+      rememberMe: true,
+      callbackURL: 'http://localhost:3000/category/01',
+    });
+
+     if (error) {
+       alert(error.message || 'Login failed. Please try again.');
+       return;
+     }
+
+     //
+     alert('Login Successfully!');
+     reset();
   };
   //console.log(errors, 'error');
   //console.log(watch('email'));
-  console.log(watch('password'));
+  //console.log(watch('password'));
   return (
     <div className="container mx-auto  flex justify-center items-center my-6 sm:my-8 lg:my-10">
       <div className="p-2 sm:p-8  rounded-xl  bg-slate-100">
@@ -65,7 +73,7 @@ const LogInPage = () => {
                 className="input w-full"
                 placeholder="Email"
               />
-             
+
               {errors.email && (
                 <p className="text-red-600 text-left italic">
                   {errors.email.message}
