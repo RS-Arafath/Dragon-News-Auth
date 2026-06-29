537e3b3b48e8eb223c476d6643721155ce1e73e4
Arafath <rsarafath.signin@gmail.com>
2026-06-29 10:43:33 +0600

    feat: update Navbar component layout and improve user logout button styling

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -15,14 +15,14 @@ const Navbar = () => {
   
   return (
     <div
-      className="flex justify-between items-center my-8 font-mystery-quest sticky top-0 left-0 py-1 w-full z-50 
+      className="flex justify-between items-center my-8  sticky top-0 left-0 py-1 w-full z-50 
 bg-white/10   backdrop-blur-xl 
 border-b border-white/10 
  
 shadow-none"
     >
       <div>{/* //fake div */}</div>
-      <div className=" ">
+      <div className="font-mystery-quest ">
         <ul className="flex justify-center items-center gap-3 md:gap-5 text-gray-900">
           <li className="hover:text-red-600  md:text-xl font-semibold  duration-200">
             <NavLink href={'/'}>Home</NavLink>
@@ -35,21 +35,27 @@ shadow-none"
           </li>
         </ul>
       </div>
-      <div className="flex gap-1 sm:gap-2 px-2 justify-center items-center">
-        {/* <CgProfile className="text-3xl" /> */}
-        <h2 className='font-semibold'>{user?.name }</h2>
-        <Image
-          src={user?.image || userAvatar}
-          width={30}
-          height={30}
-          alt="user_img"
-          className="rounded-full object-cover w-10 h-10"
-        ></Image>
+      {user ? (
+        <div className="flex gap-1 sm:gap-2 px-2 justify-center items-center">
+          {/* <CgProfile className="text-3xl" /> */}
+          <h2 className="font-semibold font-poppins">{user?.name}</h2>
+          <Image
+            src={user?.image || userAvatar}
+            width={30}
+            height={30}
+            alt="user_img"
+            className="rounded-full object-cover w-10 h-10"
+          ></Image>
 
-        <button className="btn bg-black px-2  sm:px-5  text-white">
-          <Link href={'/login'}>LogIn</Link>
+          <button className="btn bg-red-600 hover:bg-red-700 px-2  sm:px-5  text-white">
+            <Link href={'/login'}>LogOut</Link>
+          </button>
+        </div>
+      ) : (
+        <button className="btn bg-black px-2  sm:px-5  text-white font-sans">
+          <Link href={'/login'}>LogOut</Link>
         </button>
-      </div>
+      )}
     </div>
   );
 };
