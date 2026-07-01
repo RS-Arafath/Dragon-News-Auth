2ecbfd1f6da89bffcb227a9a94c69c8d85cd9f39
Arafath <rsarafath.signin@gmail.com>
2026-07-01 20:33:03 +0600

    refactor: update BreakingNews component to use AnimatedButton and modify proxy function for redirection

--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -1,14 +1,12 @@
 import Marquee from 'react-fast-marquee';
-
+import AnimatedButton from '@/components/ui/animated-button';
 const BreakingNews = () => {
   return (
     <div className=" flex justify-between  items-center bg-gray-200  p-3">
-      <button className="btn bg-red-600 text-white">Latest News</button>
-      <Marquee
-     
-        direction="left"
-      pauseOnHover={true}
-      >
+      <AnimatedButton className="btn bg-red-600 text-white">
+        Breaking News
+      </AnimatedButton>
+      <Marquee direction="left" pauseOnHover={true}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae qui
         dolores quibusdam, deleniti quae sit, ullam accusantium atque vero ipsa
         perspiciatis a vel soluta repellendus non nam optio nostrum distinctio
